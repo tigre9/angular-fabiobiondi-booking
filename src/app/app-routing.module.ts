8e792d99bb5fb43da8d625abd904e3050831d2c2
Daniele <tigre9@gmail.com>
2020-10-25 17:49:55 +0100

    Creazione della sezione search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchComponent} from "./features/search/search.component";
+import {LoginComponent} from "./features/login/login.component";
+import {CartComponent} from "./features/cart/cart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '**', redirectTo: 'search'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './features/search/search.component';
+import { CartComponent } from './features/cart/cart.component';
+import { LoginComponent } from './features/login/login.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { IntToArrayPipe } from './features/pipes/int-to-array.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CartComponent,
+    LoginComponent,
+    IntToArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
