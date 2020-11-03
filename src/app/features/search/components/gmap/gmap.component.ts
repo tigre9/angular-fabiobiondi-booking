47d1b0fdacd21f17772008bf7691412de0816af4
tigre9 <47818130+tigre9@users.noreply.github.com>
2020-11-03 17:48:29 +0100

    Update gmap.component.ts

--- src/app/features/search/components/gmap/gmap.component.ts
+++ src/app/features/search/components/gmap/gmap.component.ts
@@ -4,7 +4,7 @@ import {Hotel} from "../../../../model/hotel";
 @Component({
   selector: 'app-gmap',
   template: `
-    <!--          <img src="https://maps.googleapis.com/maps/api/staticmap?center={{active?.city}}&zoom=15&size=700x200&key=AIzaSyBYvPZPIFB4TYsRFl6TF3QIbxghz_p3ito"  width="100%" height="200">-->
+    <!--          <img src="https://maps.googleapis.com/maps/api/staticmap?center={{active?.city}}&zoom=15&size=700x200&key="  width="100%" height="200">-->
     <div class="address">
       <div class="font-big">{{active?.name}}</div>
       <div class="font-small">{{active?.location.address}}</div>
